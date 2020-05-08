e3f5a77d773e143c03fe397d58273ef348f5eb0f
Dou-baoer <xiejincumt@hotmail.com>
2020-05-08 23:46:35 +0200

    initial commit"Class002"

--- ex40a.py
+++ ex40a.py
@@ -1,9 +1,5 @@
-mystuff = {'apple': "I AM APPLES!"}
-print(mystuff['apple'])
-
 #this goes in mystuff.py
 def apple():
 	print("I AM APPLES!")
 
-import mystuff
-mystuff.apple()
+tangerine = "Living reflection of a dream"
---
+++ ex40b.py
@@ -0,0 +1,4 @@
+import ex40a
+ex40a.apple()
+
+print(ex40a.tangerine)
