b1265706a6f9e78b62a6d319cc323275fdfa3c41
Dou-baoer <xiejincumt@hotmail.com>
2020-05-11 23:19:12 +0200

    version 1.1

--- oop.test.py
+++ oop.test.py
@@ -1,4 +1,5 @@
 import random
+# random is a modul for to inport random
 from urllib.request import urlopen
 import sys
 
@@ -27,10 +28,13 @@ else:
 	PHRASE_FIRST = False
 
 # load up the words from the website
+# str.strip() is del the begin and end from the str.
 for word in urlopen(WORD_URL).readlines():
 	WORDS.append(str(word.strip(), encoding = "utf-8"))
 
-
+# str.capitalize() is change the first letter from lower case to capital and all of others to lower case.
+# .count() is the fuction to count the number of the ().
+# random.sample() is to take N element from the first parameter. acount is the second parameter.
 def convert(snippet, phrase):
 	class_names = [w.capitalize() for w in
 					random.sample(WORDS, snippet.count("%%%"))]
@@ -38,6 +42,9 @@ def convert(snippet, phrase):
 	results = []
 	param_names = []
 
+# range() is diferent from list(). range is return to the object. but list ist return to the list.
+# random.randint is to creat a random int.
+# str.join is take the ',' in all of the (element)
 	for i in range(0, snippet.count("@@@")):
 		param_count = random.randint(1,3)
 		param_names.append(','.join(
@@ -46,6 +53,7 @@ def convert(snippet, phrase):
 	for sentence in snippet, phrase:
 		result = sentence[:]
 
+# str.replace(old, new, max)
 		# fake class names
 		for word in class_names:
 			result = result.replace("%%%", word, 1)
@@ -68,6 +76,8 @@ try:
 	while True:
 		snippets = list(PHRASES.keys())
 		random.shuffle(snippets)
+# random.shuffle() is to mess up the order
+# list(dict.keys()) return to a lterate object than use a list function
 
 		for snippet in snippets:
 			phrase = PHRASES[snippet]
